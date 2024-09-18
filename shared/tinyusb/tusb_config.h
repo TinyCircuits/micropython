7566af80c705a4c819b7557476b7b7ed810e294c
Jason Marcum <info@tinycircuits.com>
2024-09-18 13:42:01 -0400

    Update

--- shared/tinyusb/tusb_config.h
+++ shared/tinyusb/tusb_config.h
@@ -43,13 +43,22 @@
 #endif
 
 #define CFG_TUH_ENABLED             1
-// #define CFG_TUH_HUB                 1
-// // max device support (excluding hub device)
-// #define CFG_TUH_DEVICE_MAX          (CFG_TUH_HUB ? 4 : 1) // hub typically has 4 ports
 
-// #define CFG_TUH_HID                 (3*CFG_TUH_DEVICE_MAX)
-// #define CFG_TUH_HID_EPIN_BUFSIZE    64
-// #define CFG_TUH_HID_EPOUT_BUFSIZE   64
+// Size of buffer to hold descriptors and other data used for enumeration
+// #define CFG_TUH_ENUMERATION_BUFSIZE 256
+#define CFG_TUH_CDC                 1 // CDC ACM
+
+
+//------------- CDC -------------//
+
+// Set Line Control state on enumeration/mounted:
+// DTR ( bit 0), RTS (bit 1)
+// #define CFG_TUH_CDC_LINE_CONTROL_ON_ENUM    0x03
+
+// Set Line Coding on enumeration/mounted, value for cdc_line_coding_t
+// bit rate = 115200, 1 stop bit, no parity, 8 bit data width
+// #define CFG_TUH_CDC_LINE_CODING_ON_ENUM   { 115200, CDC_LINE_CODING_STOP_BITS_1, CDC_LINE_CODING_PARITY_NONE, 8 }
+
 
 
 
